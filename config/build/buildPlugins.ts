750b06342315b1fc60af4f4ce288535ec7e0fb86
Illouminus <professionnel.baillot@gmail.com>
2023-02-07 15:47:54 +0100

    decomposate config - add build modes

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+
+export function buildPlugins ({path}: BuildOptions): webpack.WebpackPluginInstance[] {
+    return [
+        new HtmlWebpackPlugin({
+            template: path.html
+        }),
+        new webpack.ProgressPlugin(),
+    ]
+}
