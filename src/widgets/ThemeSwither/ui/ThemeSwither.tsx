595b4d8afc553a0d33a2bbc4d9dc8516cb1802b5
Illouminus <professionnel.baillot@gmail.com>
2023-02-15 10:55:09 +0100

    eslint i18next adding

--- src/widgets/ThemeSwither/ui/ThemeSwither.tsx
+++ src/widgets/ThemeSwither/ui/ThemeSwither.tsx
@@ -1,27 +1,24 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './ThemeSwither.module.scss';
-import React from "react";
-import {Theme, useTheme} from "app/providers/ThemeProvider";
-import LightIcon from 'shared/assets/icons/theme-light.svg'
-import DarkIcon from 'shared/assets/icons/theme-dark.svg'
-import {Button} from "shared/ui/Button";
-import {ThemeButton} from "shared/ui/Button/ui/Button";
-
+import { classNames } from 'shared/lib/classNames/classNames';
+import React from 'react';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import { Button } from 'shared/ui/Button';
+import { ThemeButton } from 'shared/ui/Button/ui/Button';
 
 interface ThemeSwitherProps {
     className?: string;
 }
 
-export const ThemeSwither = ({className}: ThemeSwitherProps) => {
-    const {theme, toggleTheme} = useTheme()
+export const ThemeSwither = ({ className }: ThemeSwitherProps) => {
+    const { theme, toggleTheme } = useTheme();
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.ThemeSwither, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
-            {theme === Theme.DARK ?  <DarkIcon /> : <LightIcon /> }
+            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon /> }
         </Button>
     );
 };
-
